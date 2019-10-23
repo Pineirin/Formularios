804230132431d0683db57bb445b59d533ac8c43f
Pablo García Marcos <uo245371@uniovi.es>
2019-10-23 20:24:36 +0200

    bbdd access

--- repositorio.js
+++ repositorio.js
@@ -1,7 +1,7 @@
 module.exports = {
     conexion : async () => {
         var mongo = require("mongodb");
-        var db = "mongodb://admin:informatica1111@cluster0-shard-00-00-ze9uo.mongodb.net:27017,cluster0-shard-00-01-ze9uo.mongodb.net:27017,cluster0-shard-00-02-ze9uo.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+        var db = "mongodb://admin:informatica1234@cluster0-shard-00-00-wykof.mongodb.net:27017,cluster0-shard-00-01-wykof.mongodb.net:27017,cluster0-shard-00-02-wykof.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
         promise = new Promise((resolve, reject) => {
             mongo.MongoClient.connect(db, (err, db) => {
                 if (err) {
